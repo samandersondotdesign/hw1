bb90f436f6ba403ee6245a5cb327da722046d9dd
samandersondotdesign <samuelanderson@me.com>
2017-01-19 17:05:53 -0800

    Create puzzle2.js

---
+++ puzzle2.js
@@ -0,0 +1,38 @@
+// Help Rudy the Red Dot collect all the green dots!
+
+// Control Rudy with the following commands:
+//  up();                   -- moves Rudy up one square
+//  down();                 -- ...down one square
+//  left();                 -- ...left one square
+//  right();                -- ...right one square
+
+// Interact with Rudy's environment using the following commands:
+//  remainingDots();        -- returns the number of green dots remaining
+//  getColor();             -- returns the color of Rudy's square
+//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
+
+function moveDown(){
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+}
+
+function moveDownAgain(){
+  down();
+  down();
+  down();
+}
+
+moveDown();
+right();
+right();
+right();
+up();
+up();
+right();
+right();
+moveDownAgain();
+
